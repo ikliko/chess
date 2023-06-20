8f2cccae163f81d1f60e28aa838d4db9f172d81e
kliko <kliko@softalix.com>
2023-06-20 19:00:21 +0300

    render figures on the board

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,50 @@ const app = new Application({
 
 const boardItems: any[][] = [];
 
+function makeFigure({
+    texture,
+    figSize,
+    boardX,
+    boardY,
+    figPaddings,
+    cellSize,
+    col,
+    row,
+}: {
+    texture: Texture;
+    figSize: number;
+    boardX: number;
+    boardY: number;
+    figPaddings: number;
+    cellSize: number;
+    col: number;
+    row: number;
+}) {
+    // console.log(texture);
+    const figure = new Sprite(texture);
+    figure.width = figSize;
+    figure.height = figSize;
+    figure.x = boardX + figPaddings + cellSize * col;
+    figure.y = boardY + figPaddings + cellSize * row;
+    // figure.on("pointerdown", function () {
+    //     // TODO: mark available places to move
+    //     // const availablePlaces = fig.getAvailablePlaces(rowI, colI);
+    //     // availablePlaces.forEach(({row, col}) => {
+    //     //     boardItems[row][col].field.texture = boardItems[row][col].texture.dark;
+    //     //     boardItems[row][col].field.interactive = true;
+    //     //     boardItems[row][col].field.buttonMode = true;
+    //     // });
+    // });
+    // figure.on("mouseover", function () {
+    // });
+    // figure.on("mouseout", function () {
+    // });
+    // figure.interactive = true;
+    // figure.buttonMode = true;
+
+    return figure;
+}
+
 function getGameplayScene() {
     const birdFromSprite = getBird();
     birdFromSprite.anchor.set(0.5, 0.5);
@@ -31,12 +75,16 @@ function getGameplayScene() {
     gameplay.interactive = true;
 
     const board = new Graphics();
-    const fullBoardSize = Math.max(app.view.width / 4, 600);
-    const cellSize = fullBoardSize / 8;
+    const fullBoardSize = Math.max(Math.round(app.view.width / 3), 400);
+    const cellSize = Math.round(fullBoardSize / 8);
+    const figSize = Math.round(cellSize * 0.7);
+    const figPaddings = Math.round((cellSize - figSize) / 2);
     const boardX = app.view.width / 2 - fullBoardSize / 2;
     const boardY = app.view.height / 2 - fullBoardSize / 2;
     board.beginFill(0xff0000);
     board.drawRect(boardX, boardY, fullBoardSize, fullBoardSize);
+    board.interactive = true;
+
     gameplay.addChild(board);
 
     const textureOrder = [
@@ -54,78 +102,108 @@ function getGameplayScene() {
     const figures = {
         knight: {
             black: {
-                availableUnits: 0,
-                shadow: Texture.from("b_knight.png"),
-                noShadow: Texture.from("b_knight_ns.png"),
+                availableUnits: 2,
+                isInitialPlace: (row: number, col: number) => (row === 0 && col === 1) || (row === 0 && col === 6),
+                normal: Texture.from("b_knight_ns.png"),
+                active: Texture.from("b_knight.png"),
             },
             white: {
-                availableUnits: 0,
-                shadow: Texture.from("w_knight.png"),
-                noShadow: Texture.from("w_knight_ns.png"),
+                availableUnits: 2,
+                isInitialPlace: (row: number, col: number) => (row === 7 && col === 1) || (row === 7 && col === 6),
+                normal: Texture.from("w_knight_ns.png"),
+                active: Texture.from("w_knight.png"),
             },
         },
         pawn: {
             black: {
-                availableUnits: 0,
-                shadow: Texture.from("b_pawn.png"),
-                noShadow: Texture.from("b_pawn_ns.png"),
+                availableUnits: 8,
+                isInitialPlace: (row: number, col: number) => row === 1,
+                normal: Texture.from("b_pawn_ns.png"),
+                active: Texture.from("b_pawn.png"),
             },
             white: {
-                availableUnits: 0,
-                shadow: Texture.from("w_pawn.png"),
-                noShadow: Texture.from("w_pawn_ns.png"),
+                availableUnits: 8,
+                isInitialPlace: (row: number, col: number) => row === 6,
+                normal: Texture.from("w_pawn_ns.png"),
+                active: Texture.from("w_pawn.png"),
             },
         },
         rook: {
             black: {
-                availableUnits: 0,
-                shadow: Texture.from("b_rook.png"),
-                noShadow: Texture.from("b_rook_ns.png"),
+                availableUnits: 2,
+                isInitialPlace: (row: number, col: number) => (row === 0 && col === 0) || (row === 0 && col === 7),
+                normal: Texture.from("b_rook_ns.png"),
+                active: Texture.from("b_rook.png"),
             },
             white: {
-                availableUnits: 0,
-                shadow: Texture.from("w_rook.png"),
-                noShadow: Texture.from("w_rook_ns.png"),
+                availableUnits: 2,
+                isInitialPlace: (row: number, col: number) => (row === 7 && col === 0) || (row === 7 && col === 7),
+                normal: Texture.from("w_rook_ns.png"),
+                active: Texture.from("w_rook.png"),
             },
         },
         queen: {
             black: {
-                availableUnits: 0,
-                shadow: Texture.from("b_queen.png"),
-                noShadow: Texture.from("b_queen_ns.png"),
+                availableUnits: 1,
+                isInitialPlace: (row: number, col: number) => row === 0 && col === 3,
+                normal: Texture.from("b_queen_ns.png"),
+                active: Texture.from("b_queen.png"),
             },
             white: {
-                availableUnits: 0,
-                shadow: Texture.from("w_queen.png"),
-                noShadow: Texture.from("w_queen_ns.png"),
+                availableUnits: 1,
+                isInitialPlace: (row: number, col: number) => row === 7 && col === 3,
+                normal: Texture.from("w_queen_ns.png"),
+                active: Texture.from("w_queen.png"),
             },
         },
         king: {
             black: {
-                availableUnits: 0,
-                isInitialPlace: (row: number, col: number) => row === 7 && col === 4,
-                shadow: Texture.from("b_king.png"),
-                noShadow: Texture.from("b_king_ns.png"),
+                availableUnits: 1,
+                isInitialPlace: (row: number, col: number) => row === 0 && col === 4,
+                normal: Texture.from("b_king_ns.png"),
+                active: Texture.from("w_king.png"),
             },
             white: {
                 availableUnits: 1,
                 isInitialPlace: (row: number, col: number) => row === 7 && col === 4,
-                shadow: Texture.from("w_king.png"),
-                noShadow: Texture.from("w_king_ns.png"),
+                normal: Texture.from("w_king_ns.png"),
+                active: Texture.from("w_king.png"),
             },
         },
         bishop: {
+            getAvailablePlaces(row: number, col: number) {
+                const possibleMoves = [];
+
+                // left up
+                if (row > 0 && col > 0) {
+                    let c = col - 1;
+                    for (let r = row - 1; r > -1 && c > -1; r--, c--) {
+                        possibleMoves.push({ row: r, col: c });
+                    }
+                }
+                // right up
+                if (row > 0 && col < 7) {
+                    let c = col + 1;
+                    for (let r = row - 1; r > -1 && c < 8; r--, c++) {
+                        possibleMoves.push({ row: r, col: c });
+                    }
+                }
+                // left down
+                // right down
+
+                return possibleMoves;
+            },
             black: {
                 availableUnits: 2,
                 isInitialPlace: (row: number, col: number) => row === 0 && (col === 2 || col === 5),
-                shadow: Texture.from("b_bishop.png"),
-                noShadow: Texture.from("b_bishop_ns.png"),
+                normal: Texture.from("b_bishop_ns.png"),
+                active: Texture.from("b_bishop.png"),
             },
             white: {
                 availableUnits: 2,
                 isInitialPlace: (row: number, col: number) => row === 7 && (col === 2 || col === 5),
-                shadow: Texture.from("w_bishop.png"),
-                noShadow: Texture.from("w_bishop_ns.png"),
+                normal: Texture.from("w_bishop_ns.png"),
+                active: Texture.from("w_bishop.png"),
             },
         },
     };
@@ -143,12 +221,43 @@ function getGameplayScene() {
             boardPath.height = cellSize;
             boardPath.x = boardX + cellSize * colI;
             boardPath.y = boardY + cellSize * rowI;
+            // for later when we activate movements
+            boardPath.on("pointerdown", function () {});
+            boardPath.on("mouseover", function () {});
+            boardPath.on("mouseout", function () {});
+            boardPath.interactive = false;
+            boardPath.buttonMode = false;
 
-            for (const figure in figures) {
-                console.log(figure);
+            let figure = null;
+
+            for (const figureKey in figures) {
+                const fig = (figures as any)[figureKey];
+                if (fig.white.availableUnits && fig.white.isInitialPlace(rowI, colI)) {
+                    figure = makeFigure({
+                        texture: fig.white.normal,
+                        figSize,
+                        boardX,
+                        boardY,
+                        col: colI,
+                        row: rowI,
+                        figPaddings,
+                        cellSize,
+                    });
+                    fig.white.availableUnits--;
+                } else if (fig.black.availableUnits && fig.black.isInitialPlace(rowI, colI)) {
+                    figure = makeFigure({
+                        texture: fig.black.normal,
+                        figSize,
+                        boardX,
+                        boardY,
+                        col: colI,
+                        row: rowI,
+                        figPaddings,
+                        cellSize,
+                    });
+                    fig.black.availableUnits--;
+                }
             }
-
-            const figure = null;
 
             boardRow.push({
                 figure,
@@ -156,16 +265,10 @@ function getGameplayScene() {
                 field: boardPath,
             });
 
-            boardPath.on("pointerdown", function () {
-                const item = boardItems[rowI - 2][colI - 1];
-                item.field.texture = item.texture.dark;
-            });
-            boardPath.on("mouseover", function () {});
-            boardPath.on("mouseout", function () {});
-            boardPath.interactive = true;
-            boardPath.buttonMode = true;
-
             gameplay.addChild(boardPath);
+            if (figure) {
+                gameplay.addChild(figure);
+            }
         }
 
         boardItems.push(boardRow);
