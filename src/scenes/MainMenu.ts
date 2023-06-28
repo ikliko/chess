687e6c5cfb5a5e382fef83039625100d90cfd965
kliko <kliko@softalix.com>
2023-06-28 22:43:44 +0300

    refactor

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -9,16 +9,23 @@ export class MainMenu extends Scene {
     protected loadScene(): void {
         const menu = new Container();
         const redRect = new Graphics();
+
         redRect
             .beginFill(0xcccccc, 0.7)
             .drawRect(0, 0, this.application.view.width, this.application.view.height)
             .endFill();
+
         menu.addChild(redRect);
 
         const playBtn = new Text("Play");
+
         playBtn.anchor.set(0.5, 0.5);
         playBtn.x = this.application.view.width / 2;
         playBtn.y = this.application.view.height / 2;
+
+        playBtn.interactive = true;
+        playBtn.buttonMode = true;
+
         playBtn.style = new TextStyle({
             fill: 0xfcfcfc,
             fontSize: 70,
@@ -31,18 +38,21 @@ export class MainMenu extends Scene {
             dropShadowColor: 0x000000,
             dropShadowDistance: 3,
         });
+
         playBtn.on("pointerdown", function () {
             menu.visible = false;
+
             window.dispatchEvent(new CustomEvent("startGame"));
         });
+
         playBtn.on("mouseover", function () {
             playBtn.style.dropShadowAngle = 10;
         });
+
         playBtn.on("mouseout", function () {
             playBtn.style.dropShadowAngle = 0.1;
         });
-        playBtn.interactive = true;
-        playBtn.buttonMode = true;
+
         menu.addChild(playBtn);
 
         this.scene = menu;
